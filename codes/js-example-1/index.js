2a915d620b41d210f642a8fe4c8c2a23e44ce2f1
Diogo Cezar <diogo@diogocezar.com>
2018-08-06 09:21:23 -0300

    Updates

--- codes/js-example-1/index.js
+++ codes/js-example-1/index.js
@@ -6,7 +6,7 @@ function getRand(){
 	return Math.floor(Math.random() * 10)
 }
 
-var n1 = getRand()
-var n2 = getRand()
+const n1 = getRand()
+const n2 = getRand()
 
 document.querySelector("#console").innerHTML = `A soma de [${n1}] com [${n2}] é = [${sumNumbers(n1, n2)}]`
